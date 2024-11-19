bb9f89648a2e75cde5e3bdc760aa20c13ef93630
zayolaura <141943599+zayolaura@users.noreply.github.com>
2024-11-19 13:24:31 -0800

    nuevaversion.py

--- hypercars.py
+++ hypercars.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Cargar el archivo Excel
-archivo_excel = 'Hypercars_2019 (3).xlsx'  # Cambia esto por el nombre de tu archivo
-df = pd.read_excel("Hypercars_2019 (3).xlsx")
+archivo_excel = 'Hypercars.xlsx'  # Cambia esto por el nombre de tu archivo
+df = pd.read_excel(archivo_excel)
 
 # Mostrar las primeras filas para verificar la estructura
 print(df.head())
@@ -12,12 +12,13 @@ print(df.head())
 plt.figure(figsize=(12, 8))
 
 # Suponemos que "Mes" es el eje X, y graficamos "Ventas1", "Ventas2", "Ventas3"
-plt.plot(df['Motor'], df['Transmision'], marker='o', linestyle='-', label='Transmision', color='b')
-"""plt.plot(df['Hypercars'], df['Caballos de fuerza'], marker='s', linestyle='-', label='Caballos de fuerza', color='g')
-plt.plot(df['Hypercars'], df['Velocidad maxima (km/h)'], marker='^', linestyle='-', label='Velocidad maxima (km/h)', color='r')"""
+plt.plot(df['Transmision'], marker='o', linestyle='-', label='Transmision', color='b')
+
+plt.plot(df['Caballos de fuerza'], marker='s', linestyle='-', label='Caballos de fuerza', color='g')
+plt.plot(df['Velocidad maxima (km/h)'], marker='^', linestyle='-', label='Velocidad maxima (km/h)', color='r')
 
 # Añadir título y etiquetas
-plt.title('Comparación de Ventas por Mes', fontsize=16)
+plt.title('Transmision', fontsize=16)
 plt.xlabel('Hypercars', fontsize=14)
 plt.ylabel('Velocidad maxima (km/h)', fontsize=14)
 plt.xticks(rotation=45)  # Rotar las etiquetas del eje X si es necesario
@@ -27,6 +28,5 @@ plt.legend()
 
 # Guardar la gráfica en un archivo (en este caso, PNG)
 plt.savefig('grafica_comparacion_ventas.png')
-
 # Mostrar la gráfica
 plt.show()
